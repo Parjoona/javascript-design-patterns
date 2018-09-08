a3ce9bf33b9bceaf1fd1e299411c73b61dcf8a89
parjoona <parjoona@hotmail.com>
2018-09-08 23:23:08 +0200

    added starter files /w functions

---
+++ global scope problem/js/script.js
@@ -0,0 +1,28 @@
+const leadself = "Me: ",
+  leadcomputer = "PC: ",
+  aSaid = ["This is a Cyber Chat"],
+  msgYes = "Yes, that's a great idea.",
+  msgNo = "No, that must be a mistake.",
+  aSassyStuff = [
+    "Like mold on books, grow myths on history.",
+    "She moved like a poem and smiled like a sphinx.",
+    "As long as we don’t die, this is gonna be one hell of a story.",
+    "She laughed, and the desert sang.",
+    "You’ve got about as much charm as a dead slug."
+  ]
+
+let talk = (msg) => {
+
+}
+
+let replyYesOrNo = () => {
+
+}
+
+let saySassyStuff = () => {
+
+}
+
+let _echo = (msg) => {
+	
+}
