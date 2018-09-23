4c7b98146a2dc5664ef968fa53369d67aedeb2bc
parjoona <parjoona@hotmail.com>
2018-09-23 17:20:48 +0200

    added abstract factory functionality

--- creational-patterns/js/factory.js
+++ creational-patterns/js/factory.js
@@ -1,19 +1,39 @@
+// More control when customizing an item
+// Easier to add more versions or updates to the object
+
 (function(win){
+    function redCircle() {
+        this.item = document.createElement('div')
+        this.item.className = 'circle red'
+    }
+
+    function blueCircle() {
+        this.item = document.createElement('div')
+        this.item.className = 'circle blue'
+    }
+
+	// Needs to be written like this to act as constructor
+    function circleFactory() {
+        this.create = (color) => {
+            return (color === 'blue') ? new blueCircle() : new redCircle()
+        }
+    }
+
 	let circleGenSingleton = (function() {
 		let instance
 
 		let init = () => {
 			let _aCircle = []
-			let _stage = document.querySelector('.advert')
+            let _stage = document.querySelector('.advert')
+            let _cf = new circleFactory()
 
 			let _position = (circle, left, top) => {
 				circle.style.left = left + 'px'
 				circle.style.top = top + 'px'
 			}
 
-			let create = (left, top) => {
-				let circle = document.createElement('div')
-				circle.className = 'circle'
+			let create = (left, top, color) => {
+				let circle = _cf.create(color).item
 				_position(circle, left, top)
 				return circle
 			}
@@ -46,7 +66,7 @@
 		let adv = document.querySelector('.advert')
 		adv.addEventListener('click', function(e) {
 			let cg = circleGenSingleton.getInstance()
-			let circle = cg.create(e.pageX-250, e.pageY-250)
+			let circle = cg.create(e.pageX-250, e.pageY-250, 'red')
 			cg.addSelf(circle)
 		})
 	  })
